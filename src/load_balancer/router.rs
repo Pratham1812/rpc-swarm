1dc7b730277a411e1c723c02f2c306ae05433379
alpha <agarwalpratham1812@gmail.com>
2025-06-09 23:06:41 +0530

    feat: basic router impl

---
+++ src/load_balancer/router.rs
@@ -0,0 +1,32 @@
+use crate::error::Result;
+use crate::load_balancer::{algorithm::LCA, endpoint::Endpoint};
+use std::sync::atomic::AtomicUsize;
+
+pub struct Router {
+    path: String,
+    network: String,
+    active_connections: AtomicUsize,
+    connections: Vec<Endpoints>,
+    algorithm: LCA,
+}
+
+impl Router {
+    pub fn new(path: String, network: String, connections: &Vec<Endpoints>) -> Result<Self> {
+        Ok(Router {
+            path,
+            network,
+            active_connections: AtomicUsize::new(0),
+            connections,
+            algorithm: LCA,
+        })
+    }
+
+    pub fn route(&self) -> String {
+        self.algorithm
+            .select_endpoint(&self.connections, self.network.clone())
+            .map_or_else(
+                || "No healthy endpoint found".to_string(),
+                |endpoint| endpoint.url.to_string(),
+            )
+    }
+}
